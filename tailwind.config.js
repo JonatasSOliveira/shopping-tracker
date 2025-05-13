a9b7766263ecd82a1c17230fcc30f6a421c8d9c8
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2025-05-13 19:31:49 -0300

    Lint fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: [
-    "./App.tsx", 
+    "./App.tsx",
     "./src/presentation/components/**/**/*.{js,jsx,ts,tsx}",
     "./src/presentation/screens/**/*.{js,jsx,ts,tsx}",
-    "./src/presentation/routes/*.{js,jsx,ts,tsx}"
-    ],
+    "./src/presentation/routes/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
